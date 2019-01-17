69cdb1f1b368844ffc3e1bc0ec7589ce805ed07b
Dmitry Borody <dmitriy.borodiy@gmail.com>
2019-01-17 12:42:19 +0700

    Add map pins

--- src/App.js
+++ src/App.js
@@ -3,9 +3,49 @@ import GoogleMap from "google-map-react";
 import styled from "styled-components";
 import "./App.css";
 
+const defaultCenter = {
+  lat: 10,
+  lng: 10
+};
+
+const markerColors = ["yellow", "green", "red", "blue", "pink"];
+
+const markers = new Array(20).fill(null).map((value, index) => ({
+  id: index,
+  text: `Marker #${index}`,
+  color: markerColors[Math.floor(markerColors.length * Math.random())],
+  lat:
+    defaultCenter.lat + Math.random() * 0.01 * (Math.random() > 0.5 ? 1 : -1),
+  lng: defaultCenter.lng + Math.random() * 0.01 * (Math.random() > 0.5 ? 1 : -1)
+}));
+
+const MarkerPinIcon = ({ color = "red" }) => (
+  <svg version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 365 560">
+    <g>
+      <path
+        fill={color}
+        d="M182.9,551.7c0,0.1,0.2,0.3,0.2,0.3S358.3,283,358.3,194.6c0-130.1-88.8-186.7-175.4-186.9
+		C96.3,7.9,7.5,64.5,7.5,194.6c0,88.4,175.3,357.4,175.3,357.4S182.9,551.7,182.9,551.7z M122.2,187.2c0-33.6,27.2-60.8,60.8-60.8
+		c33.6,0,60.8,27.2,60.8,60.8S216.5,248,182.9,248C149.4,248,122.2,220.8,122.2,187.2z"
+      />
+    </g>
+  </svg>
+);
+
+const MarkerPinWrapper = styled.div`
+  width: 20px;
+  height: 20px;
+`;
+
+const Marker = ({ text, color }) => (
+  <MarkerPinWrapper>
+    <MarkerPinIcon color={color} />
+  </MarkerPinWrapper>
+);
+
 const GoogleMapWrapper = styled.div`
-  width: 500px;
-  height: 300px;
+  width: 100%;
+  height: 100vh;
 `;
 
 class App extends Component {
@@ -13,7 +53,17 @@ class App extends Component {
     return (
       <div className="App">
         <GoogleMapWrapper>
-          <GoogleMap defaultZoom={11} defaultCenter={{ lat: 10, lng: 10 }} />
+          <GoogleMap defaultZoom={14} defaultCenter={defaultCenter}>
+            {markers.map(marker => (
+              <Marker
+                key={marker.id}
+                color={marker.color}
+                text={marker.text}
+                lat={marker.lat}
+                lng={marker.lng}
+              />
+            ))}
+          </GoogleMap>
         </GoogleMapWrapper>
       </div>
     );
